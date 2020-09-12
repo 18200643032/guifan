a5aa67671ce7996bb8492c598edc6c0e4a027881
18200643032 <910386943@qq.com>
2020-09-12 16:36:24 +0800

    t1

--- auto_run_new.py
+++ auto_run_new.py
@@ -88,7 +88,7 @@ class Auto_run(unittest.TestCase):
         os.system(f"bash /zhengzhong/sh/zz.sh {Terminal.main_free_num1} &")
         time.sleep(10)
         code,pid = runcmd("pidof test-ji-api")
-        print("pid")
+        print(pid)
         write_top("接口1资源占用情况"+str(pid)+ '\n')
         for i in range(10):
             time.sleep(120)
