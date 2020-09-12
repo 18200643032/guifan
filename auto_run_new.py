3388cab0a1f77e22024cbecabb5356591deb1a1b
18200643032 <910386943@qq.com>
2020-09-12 14:55:10 +0800

    t1

--- auto_run_new.py
+++ auto_run_new.py
@@ -83,42 +83,38 @@ class Auto_run(unittest.TestCase):
 
 
 
-def top_free():
-    """测试接口1和接口5是否存在内存显存泄露"""
-    os.system(f"bash /zhengzhong/sh/zz.sh {Terminal.main_free_num1} &")
-    code,pid = runcmd("pidof test-ji-api")
-    write_top("接口1资源占用情况"+str(pid)+ '\n')
-    for i in range(10):
-        time.sleep(120)
-        cmd_cpu = "top -n 1 -p %s |grep test  |awk '{print $10}'"%pid
-        cmd_mem = "top -n 1 -p %s |grep test  |awk '{print $11}'"%pid
-        cmd_nvidia = "nvidia-smi |grep Default |awk '{print $9}'|awk 'NR==1'"
-        code,nvidia= runcmd(cmd_nvidia)
-        code,cpu = runcmd(cmd_cpu)  #cpu占用
-        code,mem = runcmd(cmd_mem)  #内存占用
-        t = time_time()               #当前时间
-        write_top("当前时间:%s \n cpu占用:%s \n 内存占用:%s \n 显存占用:%s"%(t,cpu,mem,nvidia))
-    os.system("kill -9 %s"%pid)
-    time.sleep(2)
-    os.system(f"bash /zhengzhong/sh/zz.sh {Terminal.main_free_num5}")
-    code, pid = runcmd("pidof test-ji-api")
-    write_top("接口5资源占用情况" + str(pid)+'\n')
-    for i in range(10):
-        time.sleep(120)
-        cmd_cpu = "top -n 1 -p %s |grep test  |awk '{print $10}'"%pid
-        cmd_mem = "top -n 1 -p %s |grep test  |awk '{print $11}'"%pid
-        cmd_nvidia = "nvidia-smi |grep Default |awk '{print $9}'|awk 'NR==1'"
-        code,nvidia= runcmd(cmd_nvidia)
-        code,cpu = runcmd(cmd_cpu)  #cpu占用
-        code,mem = runcmd(cmd_mem)  #内存占用
-        t = time_time()               #当前时间
-        write_top("当前时间:%s \n cpu占用:%s \n 内存占用:%s \n 显存占用:%s"%(t,cpu,mem,nvidia))
-        def tearDown(self):
-            self.test000093_top_free()
+    def test000094_top_free(self):
+        """测试接口1和接口5是否存在内存显存泄露"""
+        os.system(f"bash /zhengzhong/sh/zz.sh {Terminal.main_free_num1} &")
+        code,pid = runcmd("pidof test-ji-api")
+        write_top("接口1资源占用情况"+str(pid)+ '\n')
+        for i in range(10):
+            time.sleep(120)
+            cmd_cpu = "top -n 1 -p %s |grep test  |awk '{print $10}'"%pid
+            cmd_mem = "top -n 1 -p %s |grep test  |awk '{print $11}'"%pid
+            cmd_nvidia = "nvidia-smi |grep Default |awk '{print $9}'|awk 'NR==1'"
+            code,nvidia= runcmd(cmd_nvidia)
+            code,cpu = runcmd(cmd_cpu)  #cpu占用
+            code,mem = runcmd(cmd_mem)  #内存占用
+            t = time_time()               #当前时间
+            write_top("当前时间:%s \n cpu占用:%s \n 内存占用:%s \n 显存占用:%s"%(t,cpu,mem,nvidia))
+        os.system("kill -9 %s"%pid)
+        time.sleep(2)
+        os.system(f"bash /zhengzhong/sh/zz.sh {Terminal.main_free_num5}")
+        code, pid = runcmd("pidof test-ji-api")
+        write_top("接口5资源占用情况" + str(pid)+'\n')
+        for i in range(10):
+            time.sleep(120)
+            cmd_cpu = "top -n 1 -p %s |grep test  |awk '{print $10}'"%pid
+            cmd_mem = "top -n 1 -p %s |grep test  |awk '{print $11}'"%pid
+            cmd_nvidia = "nvidia-smi |grep Default |awk '{print $9}'|awk 'NR==1'"
+            code,nvidia= runcmd(cmd_nvidia)
+            code,cpu = runcmd(cmd_cpu)  #cpu占用
+            code,mem = runcmd(cmd_mem)  #内存占用
+            t = time_time()               #当前时间
+            write_top("当前时间:%s \n cpu占用:%s \n 内存占用:%s \n 显存占用:%s"%(t,cpu,mem,nvidia))
 
 
 
 if __name__ == "__main__":
     unittest.main()
-    time.sleep(10)
-    top_free()
--- utls/subprocess_test.py
+++ utls/subprocess_test.py
@@ -2,11 +2,11 @@ import subprocess
 def runcmd(command,environment="export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:/usr/local/nvidia/lib:/usr/local/nvidia/lib64"):
     # command = command.split(";")
     # command.insert(0,environment)
-    res_p = subprocess.Popen(f"{environment};{command}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+    res_p = subprocess.Popen(f"{environment};{command}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = res_p.communicate()
     returncode = res_p.returncode
     if returncode == 0:
-        if stdout.endswith('\n'):
+        if stdout.decode('utf-8').endswith('\n'):
             return True, stdout.replace('\n', '')
         return True, stdout
     else:
