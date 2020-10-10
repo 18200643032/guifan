e2ee5308974c5983e0a5d48f1e7453841368ed0c
18200643032 <910386943@qq.com>
2020-10-10 14:31:52 +0800

    t1

--- auto_run_new.py
+++ auto_run_new.py
@@ -21,35 +21,35 @@ class Auto_run(unittest.TestCase):
             json.dump(json_str, f2)
     def reduction_config(self):
         runcmd("cp /zhengzhong/config/algo_config.json /usr/local/ev_sdk/config/")
-    def atest00001_not_function(self):
+    def test00001_not_function(self):
         """验证未授权返回-999"""
         code,connet = runcmd(f"bash /zhengzhong/sh/zz.sh {Terminal.main_not_function}")
         write_res(connet+'\n')
-    def atest00002_yes_function(self):
+    def test00002_yes_function(self):
         """授权"""
         code, connet = runcmd(f"bash /zhengzhong/sh/zz.sh {Terminal.main_yes_function}")
         write_res(connet + '\n')
-    def atest00003_ev_license(self):
+    def test00003_ev_license(self):
         """ev_license版本是否一致"""
         code,connet = runcmd(f"bash /zhengzhong/sh/zz.sh {Terminal.main_ev_license}")
         write_res(connet + '\n')
-    def atest00004_project_path(self):
+    def test00004_project_path(self):
         """验证工程路径与规范一致"""
         code,connet = runcmd(f"bash /zhengzhong/sh/zz.sh {Terminal.main_project_path}")
         write_res(connet + '\n')
-    def atest00005_make_file(self):
+    def test00005_make_file(self):
         """验证test.cpp和makefile"""
         code, connet = runcmd(f"bash /zhengzhong/sh/zz.sh {Terminal.main_make_file}")
         write_res(connet + '\n')
-    def atest00006_catalogue(self):
+    def test00006_catalogue(self):
         """test-ji-api和license.txt移动到任意目录，都需要能够正常运行目录"""
         code,connet = runcmd(f"bash /zhengzhong/sh/zz.sh {Terminal.main_catalogue}")
         write_res(connet + '\n')
-    def atest00007_libji_connect(self):
+    def test00007_libji_connect(self):
         """libjo.so链接所有库"""
         code,connet = runcmd(f"bash /zhengzhong/sh/zz.sh {Terminal.main_libji_connect}")
         write_res(connet + '\n')
-    def atest00008_verification_pem(self):
+    def test00008_verification_pem(self):
         """# 公私钥位置，名称验证"""
         code,connet = runcmd(f"bash /zhengzhong/sh/zz.sh {Terminal.main_verification_pem}")
         write_res(connet + '\n')
@@ -75,7 +75,7 @@ class Auto_run(unittest.TestCase):
                 file_name += a + "_" + str(b)
             code,conten = runcmd(f"bash /zhengzhong/sh/zz.sh {Terminal.main_run_sdk_dynamiv} {file_name} {config[con]}")
             write_res("动态传参"+conten + '\n')
-    def atest000093_function(self):
+    def test000093_function(self):
         """实现的接口测试"""
         for i in range(1,5):
             code,conten=runcmd(f"bash /zhengzhong/sh/zz.sh {Terminal.main_function} {i}")
